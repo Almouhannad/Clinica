c85760652d6daec9c3d60a5aeacfde2550e1f5de
Almouhannad <almouhannad.hafez@gmail.com>
2024-09-23 22:13:08 +0300

    (B) Add repo. pattern

---
+++ Clinica.Backend/SharedKernel/RepositroyPattern/IRepository.cs
@@ -0,0 +1,36 @@
+﻿using SharedKernel.Entities;
+using SharedKernel.ResultPattern;
+
+namespace SharedKernel.RepositroyPattern;
+
+public interface IRepository<TEntity>
+        where TEntity : Entity
+{
+    // CRUD operations
+
+    #region Create operation
+
+    public Task<Result<TEntity>> CreateAsync(TEntity entity);
+
+    #endregion
+
+    #region Read operations
+
+    public Result<TEntity> GetById(Guid id);
+
+    public Result<ICollection<TEntity>> GetAll();
+
+    #endregion
+
+    #region Update oprtation
+
+    public Task<Result> UpdateAsync(TEntity entity);
+
+    #endregion
+
+    #region Delete operation
+
+    public Task<Result> DeleteAsync(TEntity entity);
+
+    #endregion
+}
