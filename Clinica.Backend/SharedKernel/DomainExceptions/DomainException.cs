7e59fe3eeacfdbbadcb8d14d49999841cc1bff68
Almouhannad <almouhannad.hafez@gmail.com>
2024-09-23 22:18:13 +0300

    (B) Add domain exceptions

---
+++ Clinica.Backend/SharedKernel/DomainExceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedKernel.DomainExceptions;
+
+public abstract class DomainException : Exception
+{
+    protected DomainException(string message) :
+        base("(Domain exception) " + message)
+    {
+    }
+}
