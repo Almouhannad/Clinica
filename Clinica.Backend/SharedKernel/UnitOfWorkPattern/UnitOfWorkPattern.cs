298d80a376570bae72df4c57d5572bf370aa98ea
Almouhannad <almouhannad.hafez@gmail.com>
2024-09-23 22:14:22 +0300

    (B) Add UOF pattern

---
+++ Clinica.Backend/SharedKernel/UnitOfWorkPattern/UnitOfWorkPattern.cs
@@ -0,0 +1,11 @@
+﻿using SharedKernel.ResultPattern;
+
+namespace SharedKernel.UnitOfWorkPattern;
+
+public interface IUnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public Task<Result> SaveChangesAsync();
+    }
+}
