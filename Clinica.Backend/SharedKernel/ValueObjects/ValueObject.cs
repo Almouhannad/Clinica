652d81dbbff3f2230c340257ccdb12ddfcdb4020
Almouhannad <almouhannad.hafez@gmail.com>
2024-09-23 22:35:53 +0300

    (B) Add value object base class to kernel

---
+++ Clinica.Backend/SharedKernel/ValueObjects/ValueObject.cs
@@ -0,0 +1,35 @@
+﻿namespace SharedKernel.ValueObjects;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetAtomicValues();
+
+    #region Equality by value
+    public bool Equals(ValueObject? other)
+    {
+        return other is not null && ValuesAreEqual(other);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ValueObject other && ValuesAreEqual(other);
+    }
+
+    private bool ValuesAreEqual(ValueObject other)
+    {
+        return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+    }
+    #endregion
+
+    #region Custom hash code
+    public override int GetHashCode()
+    {
+        return GetAtomicValues()
+            .Aggregate(
+            default(int),
+            HashCode.Combine);
+    }
+
+    #endregion
+
+}
