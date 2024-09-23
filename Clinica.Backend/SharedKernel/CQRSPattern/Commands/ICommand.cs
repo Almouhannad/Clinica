7fe34d5b1a84cb25d73eb122d50815d4a17c3a7a
Almouhannad <almouhannad.hafez@gmail.com>
2024-09-23 22:22:03 +0300

    (B) Add CQRS pattern

---
+++ Clinica.Backend/SharedKernel/CQRSPattern/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SharedKernel.ResultPattern;
+
+namespace SharedKernel.CQRSPattern.Commands;
+
+// No response
+public interface ICommand : IRequest<Result>
+{
+}
+
+// With response
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ Clinica.Backend/SharedKernel/CQRSPattern/Commands/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SharedKernel.ResultPattern;
+
+namespace SharedKernel.CQRSPattern.Commands;
+
+// No response
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
+{
+}
+
+// With response
+public interface ICommandHandler<TCommand, TResponse>
+    : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
